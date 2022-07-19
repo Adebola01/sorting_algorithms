043f91503f9fe6da0d336c9a0111ff1d05c3981f
Adebola Suleiman <78347541+Adebola01@users.noreply.github.com>
2022-07-19 15:35:42 +0100

    Sorting algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,41 @@
 #include "sort.h"
 
 /**
- * shell_sort - sort algorithm
- * @array: array to sort
+*swap - the positions of two elements into an array
+*@array: array
+*@item1: array element
+*@item2: array element
+*/
+void swap(int *array, int item1, int item2)
+{
+
+	int tmp;
+
+	tmp = array[item1];
+	array[item1] = array[item2];
+	array[item2] = tmp;
+}
+/**
+ * shell_sort - function that sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
  * @size: size of the array
+ * @array: list with numbers
  */
 void shell_sort(int *array, size_t size)
 {
-int inner, outer;
-int valueToInsert;
-int interval = 1;
-int i = 0;
+	size_t gap = 1, i, index = 0;
 
-while(interval <= (int) size/3)
-	interval = interval*3 +1;
-
-
-while(interval > 0) {
-
-	for(outer = interval; outer < (int) size; outer++) {
-		valueToInsert = array[outer];
-		inner = outer;
-
-		while(inner > interval -1 && array[inner - interval] >= valueToInsert) {
-		array[inner] = array[inner - interval];
-		inner -=interval;
-		}
-
-		array[inner] = valueToInsert;
+	if (array == NULL || size < 2)
+		return;
+	while (gap < size / 3)
+		gap = 3 * gap + 1;
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+			for (index = i; index >= gap &&
+			 (array[index] < array[index - gap]); index -= gap)
+				swap(array, index, index - gap);
+		print_array(array, size);
+		gap /= 3;
 	}
-
-interval = (interval -1) /3;
-print_array(array, size);
-i++;
-}
 }
